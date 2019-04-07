aab44b53d99fca62eca772cf75de27c1d8aa988f
AronEekma <aroneekma123@gmail.com>
2019-04-07 14:37:50 +0200

    First Commit

---
+++ starter.bat
@@ -0,0 +1,6 @@
+echo "# AronEekma.github.io" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/AronEekma/AronEekma.github.io.git
+git push -u origin master
